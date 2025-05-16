a04773e9aa9f4ce460c9f61d52dcd71b15e8b906
manpreetsingh153 <www.mannusingh.2002@gmail.com>
2025-05-16 21:51:44 +0530

    databaseupdate

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -12,9 +12,9 @@ import { NextResponse } from "next/server";
 // }
 export async function POST(request: Request) {
     const body = await request.json();
-    const { name, email, password, role } = body;
+    const { fullName, email, password, role } = body;
     // Validate input data
-    if (!name || !email || !password) {
+    if (!fullName || !email || !password) {
         return NextResponse.json({ error: 'User registration failed' }, { status: 400 })
 
     }
@@ -37,7 +37,7 @@ export async function POST(request: Request) {
     const hashedPassword = await bcrypt.hash(password, 10)
     const userCreated = await prisma.user.create({
         data: {
-            name,
+            name: fullName,
             email,
             password: hashedPassword,
             role: role || 'STUDENT', // Default to STUDENT if no role is provided
